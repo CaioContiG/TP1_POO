252c8d0c1e5b22d830b351a856e228cc7202381e
CaioContiG <41450841+CaioContiG@users.noreply.github.com>
2018-11-01 16:24:11 -0300

    Delete main.cpp

--- teste/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-using namespace std;
-
-int main(int argc, char** argv) {	
-	cout<< "meu pau na sua mao" << endl;
-	return 0;
-}
