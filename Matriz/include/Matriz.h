a815ee121ac4226fbc800459eea53de69b9c7ad0
CaioContiG <41450841+CaioContiG@users.noreply.github.com>
2018-11-02 18:23:13 -0300

    Pasta da matriz adicionada

---
+++ Matriz/include/Matriz.h
@@ -0,0 +1,17 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+
+class Matriz
+{
+    public:
+        Matriz();
+        virtual ~Matriz();
+        Matriz(const Matriz& other);
+
+    protected:
+
+    private:
+};
+
+#endif // MATRIZ_H
---
+++ Matriz/src/Matriz.cpp
@@ -0,0 +1,16 @@
+#include "Matriz.h"
+
+Matriz::Matriz()
+{
+    //ctor
+}
+
+Matriz::~Matriz()
+{
+    //dtor
+}
+
+Matriz::Matriz(const Matriz& other)
+{
+    //copy ctor
+}
